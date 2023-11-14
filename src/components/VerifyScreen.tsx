9c7a5219c8f3a88411658dd7a1d71cc62821cbbc
Mark Berkeland <mberkeland@gmail.com>
2023-11-13 19:45:24 -0800

    Update VerifyScreen.tsx

--- src/components/VerifyScreen.tsx
+++ src/components/VerifyScreen.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
-import {StackScreenProps} from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {getDeviceToken} from '../utils/deviceUtil';
-import {styles} from '../public/styles';
+import { getDeviceToken } from '../utils/deviceUtil';
+import { styles } from '../public/styles';
 
-import {SERVER_BASE_URL} from '@env';
+import { SERVER_BASE_URL } from '@env';
 
 const VerifyScreen = ({
   navigation,
   route,
-}: StackScreenProps<{HomeScreen: any}>) => {
+}: StackScreenProps<{ HomeScreen: any }>) => {
   const [pin, setPin] = useState('');
   const [isPinValidState, setIsPinValidState] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
@@ -22,53 +22,60 @@ const VerifyScreen = ({
       setIsPinValidState(false);
     }
   }, [pin]);
-
   const verifyHandler = async () => {
-    const body = {request_id: route?.params?.requestId, pin: pin};
-    const deviceToken = await getDeviceToken();
-    const response = await fetch(`${SERVER_BASE_URL}/verify`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-        'silent-auth': deviceToken.token,
-        'device-id': deviceToken.deviceId,
-      },
-    });
+      const body = {
+        request_id: route?.params?.requestId,
+        pin: pin,
+      };
+      console.log(`pin in verify from login code: ${pin}`);
+      const deviceToken = await getDeviceToken();
+      const response = await fetch(`${SERVER_BASE_URL}/verify`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json',
+          'silent-auth': deviceToken.token,
+          'device-id': deviceToken.deviceId,
+        },
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (response.status === 200) {
-      console.log('Verified! Go to Secure Page!!');
-      setPin('');
+      if (response.status === 200) {
+        console.log('Verified! Go to Secure Page!!');
+        setPin('');
 
-      try {
-        await AsyncStorage.setItem('@auth', data.token);
-        navigation.navigate('Secure');
-      } catch (e) {
-        console.log(e);
+        try {
+          await AsyncStorage.setItem('@auth', data.token);
+          navigation.navigate('Secure');
+        } catch (e) {
+          console.log(e);
+        }
+      } else if (response.status === 400) {
+        console.log('Verification Pin incorrect!!');
+        setErrorMessage('Incorrect pin entered. Please retry');
+        setPin('');
+      } else if (response.status === 409) {
+        console.log("Workflow doesn't require a pin.");
+        setErrorMessage(
+          'A pin is not required for this step of the verification process.',
+        );
+        setPin('');
+      } else {
+        console.log('Verification does not exist or has expired!!');
+        setPin('');
+        navigation.navigate('Login', { errorMessage: data?.error });
       }
-    } else if (response.status === 400) {
-      console.log('Verification Pin incorrect!!');
-      setErrorMessage('Incorrect pin entered. Please retry');
-      setPin('');
-    } else if (response.status === 409) {
-      console.log("Workflow doesn't require a pin.");
-      setErrorMessage(
-        'A pin is not required for this step of the verification process.',
-      );
-      setPin('');
-    } else {
-      console.log('Verification does not exist or has expired!!');
-      setPin('');
-      navigation.navigate('Login', {errorMessage: data?.error});
-    }
   };
 
   return (
     <View style={styles.view}>
-      <Text style={styles.heading}>Welcome to the Vonage SilentAuth Demo Application</Text>
-      <Text style={styles.subHeading}>
+      <Image source={require('../assets/vonage.png')} style={styles.Image} />
+      <Text style={styles.heading}>Welcome to the</Text>
+      <Text style={styles.heading2}>Vonage SilentAuth</Text>
+      <Text style={styles.heading2}>Demo Application</Text>
+      <Text style={styles.heading2}></Text>
+      <Text style={styles.heading2}>
         Please enter your verification code to continue.
       </Text>
       <Text style={styles.errorText}>{errorMessage}</Text>
@@ -82,7 +89,9 @@ const VerifyScreen = ({
         value={pin}
       />
 
-      <TouchableOpacity onPress={verifyHandler} style={[styles.button, styles.enabledButton]}>
+      <TouchableOpacity
+        onPress={verifyHandler}
+        style={[styles.button, styles.enabledButton]}>
         <Text style={styles.buttonText}>Verify Me!</Text>
       </TouchableOpacity>
     </View>
