53029131e31bfeeafb8475cfa1e68b05ebfacb55
iujielim <iujie.lim@vonage.com>
2025-04-16 17:37:09 +0800

    upgrade react native version to 0.73

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -6,6 +6,8 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+import {it} from '@jest/globals';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
--- metro.config.js
+++ metro.config.js
@@ -4,15 +4,8 @@
  *
  * @format
  */
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const config = {};
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  "resetCache": true
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
