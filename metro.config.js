a1990b8233c72c43ae8cd1c1c9efd126bf29c74c
iujielim <iujie.lim@vonage.com>
2025-04-16 17:36:10 +0800

    upgrade react native version to 0.73

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,8 @@
  *
  * @format
  */
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const config = {};
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
